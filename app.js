89a8fa92c1bc967d8c0b92ce1ecabed2d5588067
Steeve Matou <steeve.matou@gmail.com>
2022-10-06 22:10:28 +0200

    dots caroussel added mobile version

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const carousselBtn = document.querySelectorAll("[data-caroussel_btn");
 const numberItemToAdd = document.querySelector("input[type=number]");
 const cartBtn = document.querySelectorAll("[data-input]");
 
-console.log(cartBtn);
-
 function changeValueCart(item) {
   item.dataset.input === "plus"
     ? numberItemToAdd.value++
@@ -21,7 +19,6 @@ function caroussel(item) {
 
   const activeSlide = slides.querySelector("[data-active]");
   let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-  console.log(newIndex);
   if (newIndex < 0) {
     newIndex = slides.children.length - 1;
   }
@@ -29,6 +26,19 @@ function caroussel(item) {
 
   slides.children[newIndex].dataset.active = true;
   delete activeSlide.dataset.active;
+
+  const activeDot = document
+    .querySelector(".caroussel__circle-container")
+    .querySelector("[data-active");
+
+  const innerCircle = document.querySelectorAll(".inner-circle");
+  let newArr = [...innerCircle].indexOf(activeDot) + offset;
+
+  innerCircle[newIndex].dataset.active = true;
+  delete activeDot.dataset.active;
+
+  console.log([...innerCircle].indexOf(activeDot));
+  console.log([...slides.children].indexOf(activeSlide));
 }
 
 // trigger caroussel
