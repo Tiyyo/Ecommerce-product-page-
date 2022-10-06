c749fd7c453c9e84228836bf96f71605944377ba
Steeve Matou <steeve.matou@gmail.com>
2022-10-06 10:06:27 +0200

    change value cart added

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const carousselBtn = document.querySelectorAll("[data-caroussel_btn");
+const numberItemToAdd = document.querySelector("input[type=number]");
+const cartBtn = document.querySelectorAll("[data-input]");
+
+console.log(cartBtn);
+
+function changeValueCart(item) {
+  item.dataset.input === "plus"
+    ? numberItemToAdd.value++
+    : numberItemToAdd.value--;
+  if (numberItemToAdd.value < 0) {
+    numberItemToAdd.value = 0;
+  }
+}
 
 function caroussel(item) {
   const offset = item.dataset.caroussel_btn === "next" ? 1 : -1;
@@ -18,8 +31,16 @@ function caroussel(item) {
   delete activeSlide.dataset.active;
 }
 
+// trigger caroussel
 carousselBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     caroussel(btn);
+  });
+});
+
+cartBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    changeValueCart(btn);
   });
 });
