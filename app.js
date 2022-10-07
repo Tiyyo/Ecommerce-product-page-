ccf5f38df9af8b178163a7d74b9180e9720e7172
Steeve Matou <steeve.matou@gmail.com>
2022-10-07 21:55:26 +0200

    mobile nav and cart

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
 const carousselBtn = document.querySelectorAll("[data-caroussel_btn");
 const numberItemToAdd = document.querySelector("input[type=number]");
 const cartBtn = document.querySelectorAll("[data-input]");
+const hamburger = document.getElementsByClassName("hamburger")[0];
+const displayCartBtn = document.querySelector("[data-cart]");
+const navLinksMobile = document.querySelector("[data-nav-mobile");
+const cart = document.querySelector("[data-display");
+const cartContent = document.querySelector("[data-content");
+
+console.log(cartContent.dataset.content);
+
+() => {
+  displayItemCart();
+};
+
+function displayNavMobile() {
+  if (navLinksMobile.dataset.navMobile === "isactive") {
+    navLinksMobile.dataset.navMobile = "isnotactive";
+  } else if (
+    cart.dataset.display === "isactive" &&
+    navLinksMobile.dataset.navMobile === "isnotactive"
+  ) {
+    cart.dataset.display = "isnotactive";
+    navLinksMobile.dataset.navMobile = "isactive";
+  } else {
+    navLinksMobile.dataset.navMobile = "isactive";
+  }
+}
+
+function displayCart() {
+  if (cart.dataset.display === "isactive") {
+    cart.dataset.display = "isnotactive";
+  } else if (navLinksMobile.dataset.navMobile === "isactive") {
+    navLinksMobile.dataset.navMobile = "isnotactive";
+    cart.dataset.display = "isactive";
+  } else {
+    cart.dataset.display = "isactive";
+  }
+}
+
+function displayItemCart() {
+  if (cartContent.dataset.content === "empty") {
+    cartContent.innerHTML = `<p class="empty">Your cart is empty.</p>`;
+  }
+}
 
 function changeValueCart(item) {
   item.dataset.input === "plus"
@@ -27,18 +69,15 @@ function caroussel(item) {
   slides.children[newIndex].dataset.active = true;
   delete activeSlide.dataset.active;
 
+  // same logic for dots
   const activeDot = document
     .querySelector(".caroussel__circle-container")
     .querySelector("[data-active");
 
   const innerCircle = document.querySelectorAll(".inner-circle");
-  let newArr = [...innerCircle].indexOf(activeDot) + offset;
 
   innerCircle[newIndex].dataset.active = true;
   delete activeDot.dataset.active;
-
-  console.log([...innerCircle].indexOf(activeDot));
-  console.log([...slides.children].indexOf(activeSlide));
 }
 
 // trigger caroussel
@@ -46,6 +85,19 @@ carousselBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     caroussel(btn);
   });
+});
+
+// display nav menu on click
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active");
+  displayNavMobile();
+});
+
+// display cart
+displayCartBtn.addEventListener("click", () => {
+  displayCart();
+  displayItemCart();
+  hamburger.classList.remove("is-active");
 });
 
 cartBtn.forEach((btn) => {
